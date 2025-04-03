b76662ee5def609de0c67e65b360606fcc53e571
Youngsik Kim <twilight@devcra.com>
2025-04-03 14:27:44 +0900

    add reset on game end

--- scripts/final_result.gd
+++ scripts/final_result.gd
@@ -13,3 +13,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_control_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			var startup = load("res://scenes/startup_screen.tscn").instantiate()
+			self.get_tree().root.add_child(startup)
+			self.get_node('/root/FinalResult').queue_free()
